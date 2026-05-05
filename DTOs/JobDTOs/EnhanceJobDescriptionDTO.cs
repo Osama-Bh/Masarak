8ac8c3a9949cf1db682245e3653f9d7e418a8d9f
Alshaj <bdalrhmnalshaj08@gmail.com>
2026-05-05 15:17:19 +0100

    make the endpoint in the jobController that enhancing the job description using AI

---
+++ DTOs/JobDTOs/EnhanceJobDescriptionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace GoWork.DTOs.JobDTOs
+{
+    public class EnhanceJobDescriptionDTO
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
