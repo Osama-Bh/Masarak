148d400c950caa822f37bb18ef267d0d41a15ecd
Osama <osama.mb7@gmail.com>
2026-06-06 23:28:33 +0300

    add migration

--- Migrations/20260606202435_AddHangfirejobIdFieldInTheJobsTable.Designer.cs
+++ Migrations/20260606202435_AddHangfirejobIdFieldInTheJobsTable.Designer.cs
@@ -4,6 +4,7 @@ using GoWork.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GoWork.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260606202435_AddHangfirejobIdFieldInTheJobsTable")]
+    partial class AddHangfirejobIdFieldInTheJobsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
